79dbf09fdfe91bdc470ee4aa85d5eb0029011b97
robertocajueiro <beto.cajueiro@gmail.com>
2021-07-29 08:01:30 -0300

    Condicional

---
+++ 02-06-condicionais/index.js
@@ -0,0 +1,13 @@
+var me = {
+    name: 'Roberto',
+    age: '18'
+}
+
+function checkAge(person){
+       
+    if(person.age === '18') {
+        console.log('Caiu dentro do bloco')
+      }
+}
+
+checkAge(me)
